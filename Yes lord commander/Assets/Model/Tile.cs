a9e9a22f691f4ddc8f72101cc69893b488b99fce
Djidjay <jgregokirk@hotmail.fr>
2020-11-24 09:37:40 +0100

    Add project

---
+++ Yes lord commander/Assets/Model/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile 
+{
+    enum TileType { Dirt, Wood, Stone, Snow};
+
+    TileType type = TileType.Dirt;
+
+
+
+}
---
+++ Yes lord commander/Assets/Model/World.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiorld : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
